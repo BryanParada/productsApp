8ae1c47cc18e5b99a974e847a7472a5a5e483364
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-28 14:34:27 -0300

    F206 - central Icon - widgets as arguments

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 class AuthBackground extends StatelessWidget { 
 
+  final Widget child;
+
+  const AuthBackground({
+    super.key,
+    required this.child
+    });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,7 +20,28 @@ class AuthBackground extends StatelessWidget {
 
         _PurpleBox(),
 
+        _HeaderIcon(),
+
+        this.child,
+
         ],
+      ),
+    );
+  }
+}
+
+class _HeaderIcon extends StatelessWidget {
+  const _HeaderIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: Icon(Icons.person_pin, color: Colors.white, size: 100,)
       ),
     );
   }
