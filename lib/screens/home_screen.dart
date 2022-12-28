3fde1e17f9dee7a45345135d10d6c88941799a96
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-28 19:47:46 -0300

    F217 - Product Screen - HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
    
@@ -7,9 +8,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+      appBar: AppBar(
+        title: Text('Products'),
       ),
+       body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ( BuildContext context, int index) => ProductCard(),
+       ),
+       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+       ),
     );
   }
 }
