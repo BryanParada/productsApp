630d082b416059bfc2d31f124b066801651f235f
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-03 10:29:42 -0300

    F229 - Load data into product screen

---
+++ lib/providers/product_form_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+
+//La clase ChangeNotifier es una clase abstracta que proporciona un mecanismo para informar
+// a otras partes de tu aplicación que un cambio ha ocurrido. Esto es útil si tienes algún estado
+// que es compartido entre diferentes widgets y necesitas informar a todos esos widgets cuando el estado ha cambiado.
+
+class ProductFormProvider extends ChangeNotifier {
+
+  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+
+  Product? product;
+  //constructor            v debe ser una copia para evitar referncia
+  ProductFormProvider( this.product);
+
+  bool isValidForm(){
+
+
+    return formKey.currentState?.validate() ?? false;
+  }
+
+}
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/providers/product_form_provider.dart';
 import 'package:products_app/services/services.dart';
 import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
@@ -12,7 +13,23 @@ class ProductScreen extends StatelessWidget {
 
     final productService = Provider.of<ProductsService>(context);
 
+    return ChangeNotifierProvider(
+      create: ( _ ) => ProductFormProvider(productService.selectedProduct),
+      child: _ProductScreenBody(productService: productService)
+      );
+  }
+}
 
+class _ProductScreenBody extends StatelessWidget {
+  const _ProductScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsService productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body:SingleChildScrollView(
         child: Column(
@@ -64,6 +81,10 @@ class ProductScreen extends StatelessWidget {
 class _ProductForm extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
+
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10),
       child: Container(
@@ -77,6 +98,12 @@ class _ProductForm extends StatelessWidget {
               SizedBox(height: 10,),
 
               TextFormField(
+                initialValue: product?.name,
+                onChanged: ( value ) => product?.name = value,
+                validator:(value) {
+                  if ( value == null || value.length < 1)
+                  return 'Name is required';
+                },
                 decoration: InputDecorations.authInputDecoration(
                   hintText: 'Name of the product',
                    labelText: 'Name: '
@@ -86,6 +113,14 @@ class _ProductForm extends StatelessWidget {
               SizedBox(height: 30,),
 
               TextFormField(
+                initialValue: '${product?.price}',
+                onChanged: ( value ) {
+                  if ( double.tryParse(value) == null){
+                    product?.price = 0;
+                  } else {
+                    product?.price = double.parse(value);
+                  }
+                }, 
                 keyboardType: TextInputType.number,
                 decoration: InputDecorations.authInputDecoration(
                   hintText: '\$150',
@@ -96,7 +131,7 @@ class _ProductForm extends StatelessWidget {
               SizedBox(height: 30,),
 
               SwitchListTile(
-                value: true,
+                value: product!.available,
                 title: Text('Available'),
                 activeColor: Colors.indigo,
                 onChanged: ( value) {
